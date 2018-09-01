a324176e9168f968711824935eea8b6830985bd7
Joanna Woszczyk <woszczyk.joanna@gmail.com>
2018-09-01 11:58:30 +0200

    Update js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-
+var button = $('.trigger');
 
 function getQuote() {
     $.getJSON(prefix + quoteUrl, createTweet);
@@ -29,12 +29,16 @@ function createTweet(input) {
         $('.quote').text(quoteText);
         $('.author').text("Author: " + quoteAuthor);
         $('.tweet').attr('href', tweet);
+        button.prop('disabled', false);
     }
 }
 
 $(document).ready(function() {
     getQuote();
-    $('.trigger').click(function() {
+
+    button.click(function() {
         getQuote();
+        button.prop('disabled', true);
     })
+    
 });
